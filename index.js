c4c5f7e2779b750ae61536e78c24a51d2ddddb30
Gobind <gobindsingh2004@gmail.com>
2022-08-05 15:46:30 -0700

    Finished E-Portfolio

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20;
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const booleanInt  = isOdd ? -1 : 1;
+      shapes[i].style.transform = `translate(${x * booleanInt}px, ${y * booleanInt}px)`;
+  }  
+
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -43,7 +55,7 @@ function contact(event) {
     });
 }
 
-function toggleModal(key) {
+function toggleModal() {
   //  toggle modal
   if (!isModalOpen) {
     document.body.classList += " modal--open";
